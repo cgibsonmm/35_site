d38987e537839c36ff2bb79f1b80dce03e382cca
Corey Gibson <cgibsonmm@gmail.com>
2017-11-19 13:06:15 +0000

    start building tests

--- test/controllers/my_profile_controller_test.rb
+++ test/controllers/my_profile_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
-class MyProfileControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get my_profile_show_url
-    assert_response :success
-  end
+# class MyProfileControllerTest < ActionDispatch::IntegrationTest
+#   test "should get show" do
+#     get my_profile_show_url
+#     assert_response :success
+#   end
 
-  test "should get edit" do
-    get my_profile_edit_url
-    assert_response :success
-  end
+#   test "should get edit" do
+#     get my_profile_edit_url
+#     assert_response :success
+#   end
 
-end
+# end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  @user_params = {'username' => 'truesurfer1989', email: 'cgibsonmm@gmail.com', pass_word: 'facebook1'}
+  
+  test "should create user" do
+    user = User.create(@user_params)
+    assert_success
+  end
 end
